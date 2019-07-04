a88faa7ffb7f1f6089af37439d039f24b3e1b0e1
Cris <crcaguilerapo@unal.edu.co>
2019-07-04 10:59:50 -0500

    resource_remove

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -23,37 +23,6 @@ class ResourcesController < ApplicationController
     @resource = Resource.new
   end
 
-  # POST /resources
-  def create
-    type = resource_params_poly[:Resourceable_Type]
-    id = resource_params_poly[:Resourceable_Id]
-    
-    case type
-    when "post"
-      @post = Post.find(id)
-      @resource = @post.resources.new(resource_params) 
-    when "pet"
-      @pet = Pet.find(id)
-      @resource = @pet.resources.new(resource_params)
-    when "organization"
-      @organization = Organization.find(id)
-      @resource = @organization.resources.new(resource_params) 
-    when "user"
-      @user = User.find(id)
-      @resource = @user.resources.new(resource_params)
-    else
-    end
-
-    if @resource.save
-      @resource.Resource_Link = 'https://petshappy2.s3-us-west-1.amazonaws.com/'+ @resource.file.key
-      @resource.filename = ActiveStorageBlob.find(ActiveStorageAttachment.all().last.id).filename
-      @resource.bytesize = ActiveStorageBlob.find(ActiveStorageAttachment.all().last.id).byte_size
-      @resource.Resource_Type = ActiveStorageBlob.find(ActiveStorageAttachment.all().last.id).content_type
-      render json: @resource, status: :created, location: @resource
-    else
-      render json: @resource.errors, status: :unprocessable_entity
-    end
-  end
 
   # PATCH/PUT /resources/1
   def update
