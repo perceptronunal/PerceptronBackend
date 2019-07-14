b3ca37a546bae71099eff9d2691420a1f7db44b2
Cris <crcaguilerapo@unal.edu.co>
2019-07-14 15:25:31 -0500

    fix

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -2,7 +2,7 @@ class OrganizationSerializer < ActiveModel::Serializer
   attributes :id,
   :Organization_Name,
   :Organization_Address,
-  #:Organization_Phone,
+  :Organization_Phone,
   :Organization_Email,
   :Organization_Website,
   :Organization_Description,
--- app/serializers/user_show_serializer.rb
+++ app/serializers/user_show_serializer.rb
@@ -1,9 +1,15 @@
 class UserShowSerializer < ActiveModel::Serializer
-  attributes :id, :User_Name, :User_Email, :User_Phone, :User_City, :pets, :profile, :resources
+  attributes :id, :User_Name, :User_Email, :User_Phone, :User_City, :pets, :interested_pets,:profile, :resources
 
   def pets
     object.connections.where(Connection_Type: 'Publicar').count
   end
+
+
+  def interested_pets
+    object.connections.where(Connection_Type: 'Interesado').count
+  end
+  
   
   def profile
     profile = object.resources.where(Resource_Type: 'profile')
